8e8cfc1325e4ce82bd8daaecb8e250de515ac6e2
Mira_F_Dino <zc15754956399@outlook.com>
2022-09-07 19:00:02 +0800

    9.6代码更新

---
+++ test9_6/test9_6/test.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+	return 0;
+}

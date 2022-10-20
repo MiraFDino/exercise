106f2c7e25eb746fa4983d7700734fbdd08db3f5
Mira_F_Dino <zc15754956399@outlook.com>
2022-10-20 11:04:07 +0800

    10.19代码更新

---
+++ test10_19/test10_19/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//struct stu
+//{
+//	char name[20];//名字
+//	int age;//年龄
+//	char sex[8];//性别
+//	float score;
+//}s1,s2,s3;
+//
+//int main()
+//{
+//	struct stu s4;
+//	struct stu s5;
+//	return 0;
+//}
+//
+//struct Point
+//{
+//	int x;
+//	int y;
+//};
+//
+//struct S
+//{
+//	char a;
+//	double b;
+//	struct Point p;
+//	char arr[10];
+//};
+//
+ 
+// 
+//int main()
+//{
+//	struct S ss = { '1',2.3,{20,12},"hello"};
+//	printf("%c\n", ss.a);
+//	printf("%lf\n", ss.b);
+//	printf("%d %d\n", ss.p.x, ss.p.y);
+//	printf("%s\n", ss.arr);
+//	ss.a = 'b';
+//	ss.b = 4.5;
+//	ss.p.x = 20;
+//	ss.p.y = 6;
+//	strcpy(ss.arr, "world");
+//	printf("%c\n", ss.a);
+//	printf("%lf\n", ss.b);
+//	printf("%d %d\n", ss.p.x, ss.p.y);
+//	printf("%s\n", ss.arr);
+//	return 0;
+//}
+//
+
+int main()
+{
+	return 0;
+}

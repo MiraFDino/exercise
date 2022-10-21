cbbc9bb3b9fcf29cb58a498cc2bd1b8227168e07
Mira_F_Dino <zc15754956399@outlook.com>
2022-10-21 17:30:39 +0800

    10.20代码更新

---
+++ test10_20/test10_20/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+//∆Ω”π
+//void my_strcpy(char* dest,char* src)
+//{
+//	int i = 0;
+//	for (i = 0; src[i] != '\0'; i++)
+//	{
+//		dest[i] = src[i];
+//	}
+//	dest[i] = '\0';
+//}
+
+//”≈ªØ
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest && src);	
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char src[] = "abcde";
+//	char dest[10] = { 0 };
+//	my_strcpy(dest, src);
+//	printf("%s\n", my_strcpy(dest, src));
+//	return 0;
+//}
+
+//int my_strlen(const char *str)
+//{
+//	assert(str != NULL);
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		str++;
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char arr[] = "123456";
+//	my_strlen(arr);
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+

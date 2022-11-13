e54273dbc96c34a7cccdd1d6246045a75079a2b8
Mira_F_Dino <zc15754956399@outlook.com>
2022-11-13 21:00:29 +0800

    指针进阶代码10.25

---
+++ test_10_25/test_10_25/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < sz - i; j++)
+//		{
+//			if (arr[j] < arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}w3qwe4p[//}
+//void print1(int* arr,int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	print1(arr,sz);   
+//	return 0;
+//}
+
+//int main()
+//{	
+//	int arr[12] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
+//	int(*p)[4];
+//	p = &arr;
+//	printf("%d\n", &p[2] - &p[0][0]);
+//	return 0;
+//}
+
+
+
+
